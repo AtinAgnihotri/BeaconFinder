d81ec8ab82a85bc04a1e49dc07ea143f79cffa5e
AtinAgnihotri <atinagnihotri@gmail.com>
2021-08-19 22:05:03 +0530

    Adds Proximity Handling

--- BeaconFinder/ViewController.swift
+++ BeaconFinder/ViewController.swift
@@ -39,7 +39,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         let status = manager.authorizationStatus
         if status == .authorizedAlways {
-            
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    print("Start Scanning")
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        // It's pretty safe to force unwrap this as this is known Apple test UUID
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) { [weak self] in
+            switch distance {
+            case .unknown:
+                self?.locationState = .unknown
+                
+            case .far:
+                self?.locationState = .far
+                
+            case .near:
+                self?.locationState = .near
+                
+            case .immediate:
+                self?.locationState = .rightHere
+                
+            default:
+                self?.locationState = .unknown
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        print("Recieving Notifs: \(beacons)")
+        
+        if let beacon = beacons.first {
+            print("Found beacon: \(beacon)")
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
         }
     }
     
